1f13fb4377c0573118bfeafb2678ce0b90a491b4
chytran <chytran@users.noreply.github.com>
2021-05-27 11:23:15 -0700

    JS set up JSON

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,1 +1,13 @@
 const recipeContainer = document.getElementById("container-recipe");
+
+async function getRandomMeal() {
+    const randomMeal = await fetch('www.themealdb.com/api/json/v1/1/random.php');
+}
+
+async function getMealById(id) {
+    const meal = await fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+}
+
+async function getMealsBySearch(term) {
+    const meals = await fetch('www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+}
