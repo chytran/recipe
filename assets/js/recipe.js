0e8c8d993b68f76667c301bdb3fa469bcafc6b7d
chytran <chytran@users.noreply.github.com>
2021-05-27 12:08:40 -0700

    Setting up Async function in js to retrieve recipes

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,13 +1,21 @@
 const recipeContainer = document.getElementById("container-recipe");
 
+getRandomMeal();
+
 async function getRandomMeal() {
-    const randomMeal = await fetch('www.themealdb.com/api/json/v1/1/random.php');
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php");
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+
+    console.log(randomMeal);
+
 }
 
 async function getMealById(id) {
-    const meal = await fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+    const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
 }
 
 async function getMealsBySearch(term) {
-    const meals = await fetch('www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+    const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
 }
