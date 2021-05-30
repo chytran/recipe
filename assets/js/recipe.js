c7311f61f5d3f746072acf3220bba06a0d6e4e42
chytran <chytran@users.noreply.github.com>
2021-05-30 11:07:18 -0700

    Getting Meals to show from Json

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -32,19 +32,23 @@ function addMeal(mealData, random = false) {
     meal.classList.add('meal');
 
     meal.innerHTML = `
-        <div class="meal__header" id="meal-header">
-            ${random ? `
-            <span class="random">Random Recipe</span>` : ''}
-            <img 
-                src="${mealData.strMealThumb}" 
-                alt="${mealData.Meal}"
-            />
-        </div>
-        <div class="meal__body" id="meal-body">
-            <h4>Veggie Veggies</h4>
-            <button class="fav-button">
-                <i class="fas fa-heart"></i>
-            </button>
+        <div class="real__recipe">
+            <div class="meal__header" id="meal-header">
+                ${random ? `
+                <span class="random">Random Recipe</span>` : ''}
+                <img 
+                    src="${mealData.strMealThumb}" 
+                    alt="${mealData.Meal}"
+                />
+            </div>
+            <div class="meal__body" id="meal-body">
+                <h4>${mealData.Meal}</h4>
+                <button class="fav-button">
+                    <i class="fas fa-heart"></i>
+                </button>
+            </div>
         </div>
     `;
+
+    recipeContainer.appendChild(meal);
 }
